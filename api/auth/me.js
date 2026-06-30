ce1eccb88847bb80010b54c8dd490e4f4bec94a8
pbfluffy <145362755+pbfluffy@users.noreply.github.com>
2026-06-30 23:16:06 +0700

    update API

--- api/auth/me.js
+++
@@ -1,7 +0,0 @@
-const { getUser } = require('../../lib/auth');
-
-module.exports = async (req, res) => {
-  const user = getUser(req);
-  if (!user) return res.status(401).json({ error: 'Not authenticated' });
-  res.status(200).json({ user });
-};
